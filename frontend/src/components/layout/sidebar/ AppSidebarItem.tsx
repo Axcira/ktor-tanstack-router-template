8db528590307e23918bbc951fa5cb31105e3fe00
naughty8020 <169634290+Naughty8020@users.noreply.github.com>
2026-05-27 14:52:12 +0900

    Refactor SidebarComponents

---
+++ frontend/src/components/layout/sidebar/ AppSidebarItem.tsx
@@ -0,0 +1,79 @@
+import {SidebarMenuButton, SidebarMenuItem, useSidebar} from "#/components/ui/sidebar.tsx";
+import {Popover, PopoverContent, PopoverTrigger} from "#/components/ui/popover.tsx";
+import PopoverMenuItems from "#/components/layout/sidebar/PopoverMenuItems.tsx";
+import {Collapsible, CollapsibleContent, CollapsibleTrigger} from "#/components/ui/collapsible.tsx";
+import {ChevronRight} from "lucide-react";
+import {Link} from "@tanstack/react-router";
+import type {MenuItem} from "#/components/layout/sidebar/MenuItems";
+import CollapsibleMenuItems from "#/components/layout/sidebar/CollapsibleMenuItems.tsx";
+
+export default function AppSidebarItem({item}: { item: MenuItem }) {
+    const {state} = useSidebar()
+    const isCollapsed = state === "collapsed"
+
+    if (item.children && item.children.length > 0) {
+        if (isCollapsed) {
+            return (
+                <SidebarMenuItem>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <SidebarMenuButton tooltip={item.text}>
+                                {item.icon && <span className="h-4 w-4 shrink-0">{item.icon}</span>}
+                            </SidebarMenuButton>
+                        </PopoverTrigger>
+                        <PopoverContent side="right" align="start" className="w-48 p-1 rounded-md　gap-1">
+                            <p className="px-2 py-1.5 text-xs font-semibold text-muted-foreground">
+                                {item.text}
+                            </p>
+                            <PopoverMenuItems items={item.children}/>
+                        </PopoverContent>
+                    </Popover>
+                </SidebarMenuItem>
+            )
+        }
+
+        return (
+            <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                        <SidebarMenuButton tooltip={item.text}>
+                            {item.icon && <span className="h-4 w-4 mr-2 shrink-0">{item.icon}</span>}
+                            <span>{item.text}</span>
+                            <ChevronRight
+                                className="ml-auto h-4 w-4 transition-transform group-data-[state=open]/collapsible:rotate-90"/>
+                        </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                        <CollapsibleMenuItems items={item.children}/>
+                    </CollapsibleContent>
+                </SidebarMenuItem>
+            </Collapsible>
+        )
+    }
+
+    return (
+        <SidebarMenuItem>
+            <SidebarMenuButton tooltip={item.text} asChild={!!item.to}>
+                {item.to ? (
+                    <Link to={item.to}>
+                        {item.icon && (
+                            <span className="h-4 w-4 group-data-[state=collapsed]:mr-0 mr-2 shrink-0">
+                                {item.icon}
+                            </span>
+                        )}
+                        <span className="group-data-[state=collapsed]:hidden">{item.text}</span>
+                    </Link>
+                ) : (
+                    <>
+                        {item.icon && (
+                            <span className="h-4 w-4 group-data-[state=collapsed]:mr-0 mr-2 shrink-0">
+                                {item.icon}
+                            </span>
+                        )}
+                        <span className="group-data-[state=collapsed]:hidden">{item.text}</span>
+                    </>
+                )}
+            </SidebarMenuButton>
+        </SidebarMenuItem>
+    )
+}
---
+++ frontend/src/components/layout/sidebar/MenuItems.tsx
@@ -0,0 +1,32 @@
+
+import * as React from "react";
+import {Bot, BookOpen, Settings2} from "lucide-react";
+
+export interface MenuItem {
+    text: string;
+    icon?: React.ReactNode;
+    children?: MenuItem[];
+    to?: string;
+}
+
+const menuItems: MenuItem[] = [
+    {
+        text: "Models",
+        icon: <Bot/>,
+        children: [
+            {
+                text: "Some Item 1",
+                to: "/playground/nav1-1",
+                children: [
+                    {text: "Sub Item 1-1", to: "/playground/nav1-1-1"},
+                    {text: "Sub Item 1-2", to: "/playground/nav1-1-2"},
+                ],
+            },
+            {text: "Some Item 2", to: "/playground/nav1-2"},
+        ],
+    },
+    {text: "Settings", to: "/settings", icon: <Settings2/>},
+    {text: "Documentation", to: "/documentation", icon: <BookOpen/>},
+]
+
+export default menuItems
