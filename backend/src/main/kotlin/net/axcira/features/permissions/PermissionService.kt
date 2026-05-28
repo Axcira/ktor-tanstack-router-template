f3d1e8ce2ee97bc8eb152007f31c7adc7bc7832d
Shieru292 <dev@shieru.net>
2026-05-28 22:29:20 +0900

    Add docs

--- backend/src/main/kotlin/net/axcira/features/permissions/PermissionService.kt
+++ backend/src/main/kotlin/net/axcira/features/permissions/PermissionService.kt
@@ -7,9 +7,28 @@ import org.jetbrains.exposed.v1.jdbc.Database
 data class RoleDTO(val id: UInt, val name: String, val description: String, val permissions: List<Permission>)
 
 class PermissionService(val database: Database) {
+    /**
+     * Get a single role by id.
+     */
     suspend fun getRoleById(id: UInt): RoleDTO = TODO()
+
+    /**
+     * Get all permissions based on the user id.
+     */
     suspend fun getPermissionsForUser(userId: UInt): List<Permission> = TODO()
+
+    /**
+     * Create a new role.
+     */
     suspend fun create(name: String, permissions: List<Permission>): RoleDTO = TODO()
+
+    /**
+     * Update an existing role.
+     */
     suspend fun update(id: UInt, name: String, permissions: List<Permission>): RoleDTO = TODO()
-    suspend fun delete(id: UInt): Int = TODO()
+
+    /**
+     * Delete a role. Since the user can't have a deleted role anymore, we need to provide fallback role id.
+     */
+    suspend fun delete(id: UInt, fallbackRoleId: UInt): Int = TODO()
 }
