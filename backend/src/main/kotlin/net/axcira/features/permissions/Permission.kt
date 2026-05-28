b003220c9cdf02786224454538234f80eb9f4707
Shieru292 <dev@shieru.net>
2026-05-28 22:58:44 +0900

    Add Administrator permission

--- backend/src/main/kotlin/net/axcira/features/permissions/Permission.kt
+++ backend/src/main/kotlin/net/axcira/features/permissions/Permission.kt
@@ -4,19 +4,41 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface Permission {
-    fun satisfies(required: Permission): Boolean = this::class == required::class
+    fun satisfies(required: Permission): Boolean {
+        return this is Administrator || check(required)
+    }
+
+    fun check(required: Permission): Boolean = this == required
+
+    /**
+     * Administrator can bypass any permission checks - be careful!
+     */
+    @Serializable
+    data object Administrator : Permission
 
     @Serializable
     data object ManageUsers : Permission
+
+    /**
+     * ManageArticles permission is a combination of all permissions that can be used to manage articles.
+     */
+    @Serializable
+    data object ManageArticles : Permission {
+        override fun check(required: Permission): Boolean {
+            return when (required) {
+                is CreateArticle, is UpdateArticle, is DeleteArticle -> true
+                else -> false
+            }
+        }
+    }
 
     @Serializable
     data object CreateArticle : Permission
 
     @Serializable
     data class UpdateArticle(val allowOthers: Boolean) : Permission {
-        override fun satisfies(required: Permission): Boolean {
+        override fun check(required: Permission): Boolean {
             if (required !is UpdateArticle) return false
-
             if (required.allowOthers && !this.allowOthers) return false
             return true
         }
@@ -24,11 +46,11 @@ sealed interface Permission {
 
     @Serializable
     data class DeleteArticle(val allowOthers: Boolean) : Permission {
-        override fun satisfies(required: Permission): Boolean {
-            if (required !is UpdateArticle) return false
-
+        override fun check(required: Permission): Boolean {
+            if (required !is DeleteArticle) return false
             if (required.allowOthers && !this.allowOthers) return false
             return true
         }
     }
 }
+
